b779e832e9f602c4c4bc263c0d13a81f35653a93
Mario Nebl <hello@mario-nebl.de>
2018-02-20 09:44:12 +0100

    Move 5.1 answer to own file

---
+++ src/5.sorting_things.1.ml
@@ -0,0 +1,33 @@
+let rec merge x y = match x, y with (* 'a list -> 'a list -> 'a list *)
+    | [], l -> l
+    | l, [] -> l
+    | hx::tx, hy::ty ->
+        if hx < hy
+            then hx :: merge tx y
+            else hy :: merge x ty;;
+
+let rec take n l = match n with
+    | 0 -> []
+    | _ -> match l with
+        | [] -> []
+        | h::t -> h :: take (n - 1) t;;
+
+let rec drop n l = match n with
+    | 0 -> l
+    | _ -> match l with
+        | [] -> []
+        | h::t -> drop (n - 1) t;;
+
+(* 5.1 - Avoid calculating List.length l / 2 twice: Assign result to half binding *)
+let split l = 
+    let half = List.length l / 2 in
+    let left = take half l in
+    let right = drop half l in
+    (left, right);;
+
+let rec msort = function
+    | [] -> []
+    | [x] -> [x]
+    | l -> match split l with
+        | (left, right) -> merge (msort left) (msort right);;
+
