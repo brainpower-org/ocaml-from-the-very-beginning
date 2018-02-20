7683fb348f674a625277a6f371762aef28b462d3
Mario Nebl <hello@mario-nebl.de>
2018-02-20 09:33:19 +0100

    Add msort implementation ('a list -> 'a list)

--- src/5.sorting_things.ml
+++ src/5.sorting_things.ml
@@ -25,4 +25,26 @@ let rec merge x y = match x, y with (* 'a list -> 'a list -> 'a list *)
             then hx :: merge tx y
             else hy :: merge x ty;;
 
-Printf.printf "merge [9; 53] [2; 6; 19]: %s" (format_int_list (merge [9; 53] [2; 6; 19]));;
+let rec take n l = match n with
+    | 0 -> []
+    | _ -> match l with
+        | [] -> []
+        | h::t -> h :: take (n - 1) t;;
+
+let rec drop n l = match n with
+    | 0 -> l
+    | _ -> match l with
+        | [] -> []
+        | h::t -> drop (n - 1) t;;
+
+let rec msort = function
+    | [] -> []
+    | [x] -> [x]
+    | l -> 
+        let half = List.length l / 2 in
+        let left = take half l in
+        let right = drop half l in
+        merge (msort left) (msort right);;
+
+Printf.printf "merge [9; 53] [2; 6; 19]: %s\n" (format_int_list (merge [9; 53] [2; 6; 19]));;
+Printf.printf "msort [9; 53; 2; 6; 19]: %s\n" (format_int_list (msort [9; 53; 2; 6; 19]));;
