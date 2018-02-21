3c7b79fafc11f8c4abe7e842c9c33037ece09a03
Mario Nebl <hello@mario-nebl.de>
2018-02-21 21:06:41 +0100

    Add inverted insertion sort implementation ('a list -> 'a list)

---
+++ src/5.sorting_things.3.ml
@@ -0,0 +1,21 @@
+(* write an inverted insertion sort *)
+
+let rec insert x l = (* 'a -> 'a list -> 'a list *)
+    match l with
+    | [] -> [x]
+    | h::t -> 
+        if x >= h
+            then x :: h :: t
+            else h :: insert x t;;
+
+let rec isort = function (* 'a list -> 'a list *)
+    | [] -> []
+    | h::t -> insert h (isort t);;
+
+
+let format_list l fn = Printf.sprintf "[%s]" (String.concat "; " (List.map fn l));;
+let format_int_list l = format_list l string_of_int;;
+let format_char_list l = format_list l (Printf.sprintf "%c");;
+
+Printf.printf "sort [53; 9; 2; 6; 19]: %s\n" (format_int_list (isort [53; 9; 2; 6; 19]));;
+Printf.printf "sort ['p'; 'i'; 'm'; 'c'; 's'; 'h']: %s\n" (format_char_list (isort ['p'; 'i'; 'm'; 'c'; 's'; 'h']));;
