784007954a6196f726fe24f8670f3f3f0cdf221d
Mario Nebl <hello@mario-nebl.de>
2018-02-22 22:10:09 +0100

    Add even implementation (int list -> bool list)

--- src/6.fns-fns-fns.0.ml
+++ src/6.fns-fns-fns.0.ml
@@ -4,4 +4,9 @@ let rec double = function (*int list -> int list*)
     | [] -> []
     | h::t -> h * 2 :: double t;;
 
+let rec even = function (*int list -> bool list*)
+    | [] -> []
+    | h::t -> (h mod 2 = 0) :: even t;;
+
 Printf.printf "%s\n" (format_int_list (double [1; 2; 3]));;
+Printf.printf "%s\n" (format_bool_list (even [1; 2; 3]));;
--- src/format.ml
+++ src/format.ml
@@ -1,2 +1,3 @@
 let format_list l fn = Printf.sprintf "[%s]" (String.concat "; " (List.map fn l));;
+let format_bool_list l = format_list l (Printf.sprintf "%b");;
 let format_int_list l = format_list l (Printf.sprintf "%d");;
