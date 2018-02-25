e02c82eaaee2f0206735073ecc87d300a03fb496
Mario Nebl <hello@mario-nebl.de>
2018-02-25 21:01:07 +0100

    Add map (('a -> 'b) -> 'a list -> 'b list) implementation

--- src/6.fns-fns-fns.0.ml
+++ src/6.fns-fns-fns.0.ml
@@ -8,5 +8,12 @@ let rec even = function (*int list -> bool list*)
     | [] -> []
     | h::t -> (h mod 2 = 0) :: even t;;
 
+let rec map f l = match l with (* (a -> 'b) -> 'a list -> 'b list *) 
+    | [] -> []
+    | h::t -> f h :: map f t;;
+
+let halve x = x / 2;;
+
 Printf.printf "%s\n" (format_int_list (double [1; 2; 3]));;
 Printf.printf "%s\n" (format_bool_list (even [1; 2; 3]));;
+Printf.printf "%s\n" (format_int_list (map halve [1; 2; 3]));;
