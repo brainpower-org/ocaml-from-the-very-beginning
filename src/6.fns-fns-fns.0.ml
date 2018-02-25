6f759c1e7319563bf297acc6d6c69e63b576c432
Mario Nebl <hello@mario-nebl.de>
2018-02-25 21:09:29 +0100

    Refactor to inline fun

--- src/6.fns-fns-fns.0.ml
+++ src/6.fns-fns-fns.0.ml
@@ -1,23 +1,15 @@
 #use "format.ml";;
 
-let rec doubles = function (*int list -> int list*)
-    | [] -> []
-    | h::t -> h * 2 :: doubles t;;
-
 let rec map f l = match l with (* (a -> 'b) -> 'a list -> 'b list *) 
     | [] -> []
     | h::t -> f h :: map f t;;
 
-let is_even x = x mod 2 = 0;; (*int -> bool*)
+let evens = map (fun x -> x mod 2 = 0);;
 
-let halve x = x / 2;;
+let doubles = map (fun x -> x * 2);;
 
-let double x = x * 2;;
-
-let evens = map is_even;;
-
-let doubles = map double;;
+let halves = map (fun x -> x / 2);;
 
 Printf.printf "%s\n" (format_int_list (doubles [1; 2; 3]));;
 Printf.printf "%s\n" (format_bool_list (evens [1; 2; 3]));;
-Printf.printf "%s\n" (format_int_list (map halve [1; 2; 3]));;
+Printf.printf "%s\n" (format_int_list (halves [1; 2; 3]));;
