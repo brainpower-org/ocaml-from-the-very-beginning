a6348100518b5303028e93ff6fb5a4a1c1e8bc23
Mario Nebl <hello@mario-nebl.de>
2018-02-25 21:06:31 +0100

    Switch to map-assisted implementations

--- src/6.fns-fns-fns.0.ml
+++ src/6.fns-fns-fns.0.ml
@@ -1,19 +1,23 @@
 #use "format.ml";;
 
-let rec double = function (*int list -> int list*)
+let rec doubles = function (*int list -> int list*)
     | [] -> []
-    | h::t -> h * 2 :: double t;;
-
-let rec even = function (*int list -> bool list*)
-    | [] -> []
-    | h::t -> (h mod 2 = 0) :: even t;;
+    | h::t -> h * 2 :: doubles t;;
 
 let rec map f l = match l with (* (a -> 'b) -> 'a list -> 'b list *) 
     | [] -> []
     | h::t -> f h :: map f t;;
 
+let is_even x = x mod 2 = 0;; (*int -> bool*)
+
 let halve x = x / 2;;
 
-Printf.printf "%s\n" (format_int_list (double [1; 2; 3]));;
-Printf.printf "%s\n" (format_bool_list (even [1; 2; 3]));;
+let double x = x * 2;;
+
+let evens = map is_even;;
+
+let doubles = map double;;
+
+Printf.printf "%s\n" (format_int_list (doubles [1; 2; 3]));;
+Printf.printf "%s\n" (format_bool_list (evens [1; 2; 3]));;
 Printf.printf "%s\n" (format_int_list (map halve [1; 2; 3]));;
