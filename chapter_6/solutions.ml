a1daf2c232b704f0bf6d6994d901d033ae50841a
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2018-09-12 17:11:23 +0200

    start with chapter 6 solutions

--- chapter_6/solutions.ml
+++ chapter_6/solutions.ml
@@ -1,49 +1,11 @@
-let rec take n l = match n with
-  | 0 -> []
-  | _ -> match l with
-    | [] -> []
-    | h::t -> h :: take (n - 1) t;;
-
-let rec drop n l = match n with
-  | 0 -> l
-  | _ -> match l with
-    | [] -> []
-    | h::t -> drop (n - 1) t;;
-
-let rec length l =
-  let rec inner_length l n =
-    match l with
-    | [] -> n
-    | h :: t -> inner_length t n + 1 in
-  inner_length l 0;;
-
-let split l = 
-  let halflen = length l / 2 in
-  ( take halflen l, drop halflen l );;
-
-let rec map f l = match l with (* (a -> 'b) -> 'a list -> 'b list *) 
+let rec double = function
   | [] -> []
-  | h::t -> f h :: map f t;;
+  | h::t ->  (h * 2) :: double t 
 
-let evens = map (fun x -> x mod 2 = 0);;
-
-let doubles = map (fun x -> x * 2);;
-
-let halves = map (fun x -> x / 2);;
-
-let rec merge cmp x y = match x, y with
-  | [], l -> l
-  | l, [] -> l
-  | hx::tx, hy::ty -> if cmp hx hy
-    then hx :: merge cmp tx (hy :: ty)
-    else hy :: merge cmp (hx :: tx) ty;;
-
-let rec msort cmp l = match l with
+let rec evens = function 
   | [] -> []
-  | [x] -> [x]
-  | _ -> let (left, right) = split l in
-    merge cmp (msort cmp left) (msort cmp right);;
+  | h::t -> (h mod 2 = 0) :: evens t
 
-let rec calm = function
-    | [] -> []
-    | h::t -> (if h = '!' then '.' else h) :: calm t;;
+let rec map f = function  
+| [] -> []
+| h::t -> f h :: map f t
--- chapter_6/test.ml
+++ chapter_6/test.ml
@@ -2,10 +2,15 @@ open Core_kernel
 open OUnit2
 open Solutions
 
+
 let tests =
   [
-    (* "not false => true">:: 
-    (fun _ -> assert_equal true (not false)); *)
+    "double [2;3;4] => [4;6;8]">:: 
+      (fun _ -> assert_equal [4;6;8] (double [2;3;4])); 
+    "evens [1;2;3] => [false;true;false]">::
+      (fun _ -> assert_equal [false;true;false] (evens [1;2;3]));
+    "map (a -> 0) [1;2;3] => [0;0;0]">::
+      (fun _ -> assert_equal [0;0;0] (map (fun a -> 0) [0;0;0]));
   ]
 
 let () =
