d19dce4d11f737266f320258c0e76a2372b5c35d
rsteube <rsteube@users.noreply.github.com>
2018-09-26 12:54:42 +0200

    added map function variants

--- chapter_6/solutions.ml
+++ chapter_6/solutions.ml
@@ -1,11 +1,21 @@
+open Base
+
 let rec double = function
   | [] -> []
   | h::t ->  (h * 2) :: double t 
 
 let rec evens = function 
   | [] -> []
-  | h::t -> (h mod 2 = 0) :: evens t
+  | h::t -> (h % 2 = 0) :: evens t
 
 let rec map f = function  
   | [] -> []
   | h::t -> f h :: map f t
+
+let map_double = map (fun i -> i * 2)
+
+let map_evens = map (fun i -> i % 2 = 0)
+
+let base_double = List.map ~f:(fun i -> i * 2)
+
+let calm l = l (* TODO implement function *)
--- chapter_6/test.ml
+++ chapter_6/test.ml
@@ -11,6 +11,14 @@ let tests =
       (fun _ -> assert_equal [false;true;false] (evens [1;2;3]));
     "map (a -> 0) [1;2;3] => [0;0;0]">::
       (fun _ -> assert_equal [0;0;0] (map (fun a -> 0) [0;0;0]));
+    "map_double [2;3;4] => [4;6;8]">:: 
+      (fun _ -> assert_equal [4;6;8] (map_double [2;3;4])); 
+    "map_evens [1;2;3] => [false;true;false]">::
+      (fun _ -> assert_equal [false;true;false] (map_evens [1;2;3]));
+    "base_double [2;3;4] => [4;6;8]">:: 
+      (fun _ -> assert_equal [4;6;8] (base_double [2;3;4])); 
+    "calm ['H';'E';'L';'P';'!';' ';'F';'I';'R';'E';'!'] => ['H';'E';'L';'P';'.';' ';'F';'I';'R';'E';'.']">:: 
+      (fun _ -> assert_equal ['H';'E';'L';'P';'!';' ';'F';'I';'R';'E';'!'] (calm ['H';'E';'L';'P';'.';' ';'F';'I';'R';'E';'.'])); 
   ]
 
 let () =
