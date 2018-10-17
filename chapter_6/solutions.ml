f485a927295eca23e43651affe949deb1570cb8a
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2018-10-17 16:59:44 +0200

    add calm variations from chapter 6

--- chapter_6/solutions.ml
+++ chapter_6/solutions.ml
@@ -8,6 +8,7 @@ let rec evens = function
   | [] -> []
   | h::t -> (h % 2 = 0) :: evens t
 
+(* ( 'a -> 'a ) -> 'a list -> 'a list  *)
 let rec map f = function  
   | [] -> []
   | h::t -> f h :: map f t
@@ -18,4 +19,27 @@ let map_evens = map (fun i -> i % 2 = 0)
 
 let base_double = List.map ~f:(fun i -> i * 2)
 
-let calm l = l (* TODO implement function *)
+ (* char list -> char list *)
+let rec calm = function
+  | [] -> []
+  | '!'::t ->  '.' :: calm t
+  | h::t -> h :: calm t
+
+let rec calm2 l = match l with 
+  | [] -> []
+  | '!'::t ->  '.' :: calm t
+  | h::t -> h :: calm t
+
+(* char list -> char list *)
+let calm_map = map( function 
+  | '!' -> '.' 
+  | h -> h )
+
+(* ( 'a -> 'a ) -> 'a list -> 'a list*)
+let calm_mappable f = map f 
+
+(* int -> int *)
+let clip = function 
+  | x when x > 10 -> 10
+  | x when x < 1 -> 1
+  | x -> x
