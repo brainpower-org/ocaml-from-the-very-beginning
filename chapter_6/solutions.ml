b69f1fcb88600b757508292f7edd41fbe4e20f67
Mario Nebl <hello@mario-nebl.de>
2018-11-07 17:52:52 +0100

    implement clip_list and apply [6.2...6.4]

--- chapter_6/solutions.ml
+++ chapter_6/solutions.ml
@@ -8,14 +8,14 @@ let rec evens = function
   | [] -> []
   | h::t -> (h % 2 = 0) :: evens t
 
-(* ( 'a -> 'a ) -> 'a list -> 'a list  *)
-let rec map f = function  
+(* f:('a -> 'a) -> 'a list -> 'a list  *)
+let rec map ~f = function  
   | [] -> []
-  | h::t -> f h :: map f t
+  | h::t -> f h :: map ~f:f t
 
-let map_double = map (fun i -> i * 2)
+let map_double = map ~f:(fun i -> i * 2)
 
-let map_evens = map (fun i -> i % 2 = 0)
+let map_evens = map ~f:(fun i -> i % 2 = 0)
 
 let base_double = List.map ~f:(fun i -> i * 2)
 
@@ -31,15 +31,33 @@ let rec calm2 l = match l with
   | h::t -> h :: calm t
 
 (* char list -> char list *)
-let calm_map = map( function 
+let calm_map = map ~f:( function 
   | '!' -> '.' 
   | h -> h )
 
 (* ( 'a -> 'a ) -> 'a list -> 'a list*)
-let calm_mappable f = map f 
+let calm_mappable f = map ~f:f 
 
 (* int -> int *)
 let clip = function 
   | x when x > 10 -> 10
   | x when x < 1 -> 1
   | x -> x
+
+(* Exercise 6.2:  int list -> int list*)
+let clip_list = List.map ~f:clip
+
+(* Exercise 6.3: int list -> int list*)
+let anon_clip_list = List.map ~f:(function 
+  | x when x > 10 -> 10
+  | x when x < 1 -> 1
+  | x -> x)
+
+(* Exercise 6.4: (α -> β) -> int -> α -> β *)
+let rec apply f count arg = 
+  match count with
+    | 1 -> f arg
+    | n -> apply f (n - 1) (f arg) 
+
+(* For the lazy: https://ocaml.janestreet.com/ocaml-core/v0.9/doc/base/Base/Fn/index.html
+let apply f n = Fn.apply_n_times ~n:n f *)
