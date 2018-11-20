86d190b10146e6f13bad1f0d7a4ff1b565007407
rsteube <rsteube@users.noreply.github.com>
2018-11-20 14:04:48 +0100

    fixed chapter 5 insertion sort
    - added tests
    - added a function to print an int list in unit tests
    - copied function/tests to chapter 6 in preparation for exercise 6.5

--- chapter_5/test.ml
+++ chapter_5/test.ml
@@ -2,10 +2,16 @@ open Core_kernel
 open OUnit2
 open Solutions
 
+let print_int_list l = List.map l ~f:Int.to_string |> String.concat ~sep:";"
+
 let tests =
   [
-    (* "not false => true">:: 
-    (fun _ -> assert_equal true (not false)); *)
+    "insertion_sort [] => []">::
+      (fun _ -> assert_equal ~printer:print_int_list [] (insertion_sort [])); 
+    "insertion_sort [1] => [1]">::
+      (fun _ -> assert_equal ~printer:print_int_list [1] (insertion_sort [1])); 
+    "insertion_sort [5;7;3;1] => [1;3;5;7]">::
+      (fun _ -> assert_equal ~printer:print_int_list [1;3;5;7] (insertion_sort [5;7;3;1])); 
   ]
 
 let () =
