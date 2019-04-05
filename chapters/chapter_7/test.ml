cae3c64f849fbc1e3330274d51804e408baed733
Mario Nebl <hello@mario-nebl.de>
2019-04-06 01:39:44 +0200

    chore: move to chapter dir

--- chapters/chapter_7/test.ml
+++ chapters/chapter_7/test.ml
@@ -1,5 +1,4 @@
 
-open Base
 open OUnit2
 open Solutions
 
@@ -8,7 +7,7 @@ let tests =
     "smallest [7; 89; 1; 2] -> 1">:: 
       (fun _ -> assert_equal 1 (smallest [7; 89; 1; 2])); 
     "smallest [] raise Not_found">::
-      (fun _ -> assert_equal (-1) (try (smallest []) with Not_found -> (-1)));
+      (fun _ -> assert_equal (-1) (try (smallest []) with Failure(_) -> (-1)));
   ]
 
 let () =
