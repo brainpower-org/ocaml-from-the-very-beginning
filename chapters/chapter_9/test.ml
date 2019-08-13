bb4bcd3449ddc92183cdd53f12f06aac494eace2
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2019-08-13 22:48:08 +0200

    resolve 9.2

---
+++ chapters/chapter_9/test.ml
@@ -0,0 +1,24 @@
+open Base
+open OUnit2
+open Solutions
+
+let print_char_list l =  List.map l ~f:Char.to_string |> String.concat ~sep:";"
+
+let tests =
+  [
+    "1) member_all 1 [[1;2;3;7;2]] -> true">:: 
+      (fun _ -> assert_equal true (member_all 1 [[1;2;3;7;2]])); 
+    "2) member_all 1 [[1;2];[3;7;2]] -> false">:: 
+      (fun _ -> assert_equal false (member_all 1 [[1;2];[3;7;2]])); 
+    "3) member_all 1 [] -> false">:: 
+      (fun _ -> assert_equal ~printer:Bool.to_string false (member_all 1 [])); 
+    "4) member_all 'a' [['a']] -> true">:: 
+      (fun _ -> assert_equal ~printer:Bool.to_string true (member_all 'a' [['a']])); 
+    "5) member_all 'c' [['a'];['b','a']] -> false">:: 
+      (fun _ -> assert_equal ~printer:Bool.to_string true (member_all 'a' [['a']])); 
+    "6) member_all 'a' [[]] -> false">:: 
+      (fun _ -> assert_equal ~printer:Bool.to_string true (member_all 'a' [['a']])); 
+  ]
+
+let () =
+  run_test_tt_main ("tests" >::: tests)
