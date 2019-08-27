b18e55dda6a2571678cc40d0c9280368d2aecebc
rsteube <rsteube@users.noreply.github.com>
2019-08-27 23:27:59 +0200

    added exercise 9.3

--- chapters/chapter_9/solutions.ml
+++ chapters/chapter_9/solutions.ml
@@ -1,3 +1,4 @@
+open List
 
 let rec member n l = match l with (* 'a -> 'a list -> bool *)
   | [] -> false
@@ -14,3 +15,13 @@ let member_all x l =
       | [] -> r
       | h::t -> member_all p (r && p h) t
     in member_all (member x) true l  
+
+
+(*map: (a->b) -> [a] -> [b]*)
+let element_division r d = d / r
+
+
+let division1 r l = map ((fun r d -> d / r) r) l
+
+let division r l = map ((fun r' -> fun d -> d / r') r) l
+
